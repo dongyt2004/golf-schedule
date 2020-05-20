9ee7c4a81521611c895fc76ffd958c00dd9d2d75
dongyt <dongyt2004@163.com>
2020-05-20 15:38:57 +0800

    5th commit

--- scheduler.js
+++ scheduler.js
@@ -44,35 +44,32 @@ scheduler.getEvent({
 // 向Cronicle添加要调度的洒水计划
 db.exec("select * from plan", [], function (plans) {
     plans.forEach(function (plan, index, arr) {
-        var cover_dates = plan.cover_date.split(",");
-        cover_dates.forEach(function (cover_date, i, arr) {
-            scheduler.getEvent({
-                title: plan.id + "-" + i
-            }).catch(function() {  // 没有这个job，则创建
-                scheduler.createEvent({
-                    title: plan.id + "-" + i,
-                    catch_up: 1,
-                    enabled: 1,
-                    category: 'general',
-                    target: 'allgrp',
-                    algo: 'round_robin',
-                    plugin: 'urlplug',
-                    params: {
-                        method: 'post',
-                        url: 'http://' + process.env.SCHEDULED_SERVICE_ID + ":" + process.env.SCHEDULED_SERVICE_PORT + '/irrigate/' + plan.id + '/' + plan.involved_nozzle + '/' + plan.how_long,
-                        success_match: '1',
-                        error_match: '0'
-                    },
-                    retries: 3,
-                    retry_delay: 30,
-                    timing: getFutureTiming(cover_date + " " + plan.start_time),
-                    timezone: 'Asia/Shanghai'
-                }).then(function() {
-                    console.log("启动时向Cronicle添加了%d球场%d区域洒水计划%s，调度时间%s", plan.course_id, plan.area_id, plan.id + "-" + i, cover_date + " " + plan.start_time);
-                }).catch(function(err) {
-                    console.log(err.code + ":" + err.message);
-                    process.exit();
-                });
+        scheduler.getEvent({
+            title: "" + plan.id
+        }).catch(function() {  // 没有这个job，则创建
+            scheduler.createEvent({
+                title: "" + plan.id,
+                catch_up: 1,
+                enabled: 1,
+                category: 'general',
+                target: 'allgrp',
+                algo: 'round_robin',
+                plugin: 'urlplug',
+                params: {
+                    method: 'post',
+                    url: 'http://' + process.env.SCHEDULED_SERVICE_ID + ":" + process.env.SCHEDULED_SERVICE_PORT + '/irrigate/' + plan.id,
+                    success_match: '1',
+                    error_match: '0'
+                },
+                retries: 3,
+                retry_delay: 30,
+                timing: getFutureTiming(plan.start_time),
+                timezone: 'Asia/Shanghai'
+            }).then(function() {
+                console.log("启动时向Cronicle添加了洒水计划%s，调度时间%s", plan.id, plan.start_time);
+            }).catch(function(err) {
+                console.log(err.code + ":" + err.message);
+                process.exit();
             });
         });
     });
